93f48374e2af7c9edb61c6b38fadf896dcea2ee7
Raahim_Ali <73752508+hahah420@users.noreply.github.com>
2025-10-23 17:53:51 +0500

    Commit

---
+++ LAB Exercises/LAB 2/Q2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    //  What does the following code print?
+    // printf( "*\n**\n***\n****\n*****\n" ); 
+    printf("*\n**\n***\n****\n*****\n");
+    return 0;
+}
+
