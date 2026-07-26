e679c214eaa70d255abc59bdb01e292f69fb718d
Raahim_Ali <73752508+hahah420@users.noreply.github.com>
2026-07-27 01:38:02 +0500

    commit

--- Array/Matrixmultiplication.c
+++ Array/Matrixmultiplication.c
@@ -3,11 +3,17 @@
 int main(){
     int row1,col1,row2,col2;
 
-    printf("Enter Row And Column Of Matrix A: ");
-    scanf("%d,%d", &row1,&col1); 
+    printf("Enter Row Of Matrix A: ");
+    scanf("%d", &row1);
     
-    printf("Enter Row And Column Of Matrix B: ");
-    scanf("%d,%d", &row2,&col2); 
+    printf("Enter column Of Matrix A: ");
+    scanf("%d", &col1);
+    
+    printf("Enter Row Of Matrix B: ");
+    scanf("%d", &row2);
+    
+    printf("Enter column Of Matrix B: ");
+    scanf("%d", &col2);
     
     int A[row1][col1];
     int B[row2][col2];
@@ -21,15 +27,36 @@ int main(){
     printf("Enter Element Of Matrix A: ");
     for(int i=0; i<row1; i++){
         for(int j=0; j<col1; j++){
-            scanf("%d",A[i][j]);
+            scanf("%d",&A[i][j]);
         }
     }
 
     printf("Enter Element Of Matrix B: ");
     for(int i=0; i<row2; i++){
         for(int j=0; j<col2; j++){
-            scanf("%d",B[i][j]);
+            scanf("%d",&B[i][j]);
         }
     }
+
+    for(int i=0; i<row1; i++){
+        for(int j=0; j<col2; j++){
+            result[i][j]=0;
+        }
+    }
+
+    for(int i=0; i<row1; i++){
+        for(int j=0; j<col2; j++){
+            for(int k=0; k<col1; k++)
+            result[i][j]+=A[i][k] * B[k][j];
+        }
+    }
+
+    for(int i=0; i<row1; i++){
+        for(int j=0; j<col2; j++){
+            printf("%d ",result[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
