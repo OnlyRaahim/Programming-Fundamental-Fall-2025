0dc6985b7bb3a441dcc4644635751d255555a7cd
Raahim_Ali <73752508+hahah420@users.noreply.github.com>
2026-08-04 06:55:33 +0500

    commit

--- Assignment/Assignment 1/Q2.c
+++ Assignment/Assignment 1/Q2.c
@@ -5,10 +5,10 @@ void registervotes(int *A,int *B,int voters){
     for(int i=1;i<=voters;i++){
 
         int choice;
-        printf("Voter Number %d\n",i);
+        printf("\nVoter Number %d\n",i);
         printf("1. Candidate A\n");
         printf("2. Candidate B\n");
-        printf("Enter Choice:\n");
+        printf("\nEnter Choice:\n");
         scanf("%d",&choice);
 
         if(choice==1){
@@ -20,13 +20,51 @@ void registervotes(int *A,int *B,int voters){
         }
 
         else{
-            printf("Inavlid Choice!!\n");
-            return 0;
+            printf("\nInavlid Choice!!\n");
         }
     }
 }
 
 void countvotes(int A,int B){
-    printf("Total Votes Of Candidate A Is: %d",A);
-    printf("Total Votes Of Candidate B Is: %d",B);
+    printf("\nTotal Votes Of Candidate A Is: %d\n",A);
+    printf("Total Votes Of Candidate B Is: %d\n",B);
+}
+
+void calculatevotepercentage(int A,int B){
+    int totalvotes=A+B;
+
+    printf("\nCandidate A: %d\n",(A*100)/totalvotes);
+    printf("Candidate B: %d\n",(B*100)/totalvotes);
+    
+}
+
+void displaywinner(int A,int B){
+    
+    if (A>B){
+        printf("\nA Is Winner\n");
+    }
+    
+    else if(B>A){
+        printf("\nB Is Winner\n");
+    }
+
+    else{
+        printf("\nBoth Candidate Have Equal Vote\n");
+    }
+}
+
+int main(){
+    int candidateA=0,candidateB=0;
+    int voters;
+
+    printf("\nEnter No Of Voters: \n");
+    scanf("%d",&voters);
+
+    registervotes(&candidateA,&candidateB,voters);
+    countvotes(candidateA,candidateB);
+    calculatevotepercentage(candidateA,candidateB);
+    displaywinner(candidateA,candidateB);
+
+    return 0;
+
 }
